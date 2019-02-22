5808ee00a14d99adb4cb73f3b3eb67219f264664
github username <github email address>
2019-02-21 16:27:54 -0800

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+class Post
+
+    attr_accessor :author, :title
+
+    # def initialize(author, title)
+    #     self.author=author
+    #     self.title=title
+    # end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+    attr_accessor :artist, :title
+
+    # def initialize(artist, title)
+    #     self.artist=artist
+    #     self.title=title
+    # end
+
+end
